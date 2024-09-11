7baea7504dad5f153b22dd65e4d8610615d73ea4
Manuel <hemmanuelmtz777@gmail.com>
2024-09-11 13:10:06 -0600

    Haciendo multiples vistas en kotlin

---
+++ app/src/main/java/com/example/myapplication1/ui/theme/screens/HomeScreen.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication1.ui.theme.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun HomeScreen(navController: NavController){
+    Column {
+     Text(text = "Esta es la pantalla de inicio")
+        Button(onClick = {navController.navigate("menu")}) {
+            
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication1/ui/theme/screens/MenuScreen.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication1.ui.theme.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun MenuScreen(navController: NavController){
+    Column {
+        Text(text = "Pantalla del menu")
+        Button(onClick = {navController.navigate("home")}) {
+            
+        }
+    }
+}
