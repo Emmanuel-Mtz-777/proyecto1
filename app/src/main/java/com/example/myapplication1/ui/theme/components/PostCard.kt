c10b43e26fe08aeaf4ca508a81d71ed1ae71f74e
Manuel <hemmanuelmtz777@gmail.com>
2024-10-15 17:31:45 -0600

    Se incluyo la funcion adaptative para adaptar el contenido a diferentes pantallas y se arregalron errores al invocar funciones

--- app/src/main/java/com/example/myapplication1/ui/theme/components/PostCard.kt
+++ app/src/main/java/com/example/myapplication1/ui/theme/components/PostCard.kt
@@ -3,9 +3,13 @@ package com.example.myapplication1.ui.theme.components
 
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -60,5 +64,46 @@ fun PostCard(id: Int, title: String, text: String, image: Painter){
             modifier = Modifier
                 .padding(10.dp)
         )
+    }
+}
+
+@Composable
+fun PostCardCompact(id: Int, title: String, text: String, image: Painter) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(3.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.Black,
+            contentColor = Color.White
+        )
+    ) {
+        Row {
+            Image(
+                modifier = Modifier
+                    .width(60.dp)
+                    .height(100.dp)
+                    .padding(5.dp),
+                painter = image,
+                contentDescription = "Cipher logo",
+                contentScale = ContentScale.FillBounds
+            )
+            Column {
+                Text(
+                    text = title,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(10.dp)
+                )
+                Text(
+                    text = text,
+                    fontSize = 10.sp,
+                    textAlign = TextAlign.Justify,
+                    modifier = Modifier
+                        .padding(10.dp)
+                )
+            }
+        }
     }
 }
--- app/src/main/java/com/example/myapplication1/ui/theme/screens/Components.kt
+++ app/src/main/java/com/example/myapplication1/ui/theme/screens/Components.kt
@@ -29,6 +29,7 @@ import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material.icons.filled.Warning
@@ -36,6 +37,9 @@ import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.Call
 import androidx.compose.material.icons.outlined.Face
 import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.AssistChip
@@ -69,6 +73,7 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -80,6 +85,7 @@ import androidx.navigation.NavController
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.withFrameNanos
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -88,13 +94,18 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.window.core.layout.WindowHeightSizeClass
+import androidx.window.core.layout.WindowWidthSizeClass
 import com.example.myapplication1.R
 import com.example.myapplication1.data.model.MenuModel
 import com.example.myapplication1.data.model.PostModel
 import com.example.myapplication1.ui.theme.components.PostCard
+import com.example.myapplication1.ui.theme.components.PostCardCompact
 import kotlinx.coroutines.launch
 import org.w3c.dom.Text
 import java.util.Calendar
+
+
 
 
 @Composable
@@ -112,6 +123,7 @@ fun ComponentScreen(navController: NavController) {
         MenuModel(10, "Snack Bars", "snack-bars", Icons.Filled.DateRange),
         MenuModel(11, "Alert Dialogs", "alert-dialogs", Icons.Filled.Menu),
         MenuModel(12, "Bars", "bars", Icons.Filled.DateRange),
+        MenuModel(12, "Adaptive", "adaptative", Icons.Filled.Phone),
 
         )
     var component by rememberSaveable { mutableStateOf("") } //Actualiza el valor de la variable
@@ -339,10 +351,11 @@ fun ComponentScreen(navController: NavController) {
             "Badges" -> Badges()
 //            "date-pickers" -> DatePickers()
 //            "time-pickers" -> TimePickers()
-            "snack-bar" -> SnackBars()
+            "snack-bars" -> SnackBars()
             "alert-dialogs" -> AlertDialogs()
             "bars" -> Bars()
-            //rutas para las demás pantallas
+            "adaptative"-> Adaptive()
+
         }
     }
 }
@@ -674,28 +687,27 @@ fun Badges(){
 //        }
 //    }
 //}
-
 @Preview(showBackground = true)
 @Composable
-fun SnackBars(){
+fun SnackBars() {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceEvenly,
-        modifier = Modifier.fillMaxSize()
-    ){
-        val snackSate = remember { SnackbarHostState() }
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        val snackState = remember { SnackbarHostState() }
         val snackScope = rememberCoroutineScope()
 
-        SnackbarHost(hostState = snackSate, Modifier)
+        SnackbarHost(hostState = snackState, Modifier)
 
-        fun launchSnackBar(){
-            snackScope.launch {
-                snackSate.showSnackbar("The message was sent")
-            }
+        fun launchSnackBar() {
+            snackScope.launch { snackState.showSnackbar("The message was sent") }
         }
 
-        Button(onClick = { launchSnackBar() }) {
-            Text(text = "Show SnackBar")
+        Button(::launchSnackBar) {
+            Text("Show Snackbar")
+
         }
     }
 }
@@ -745,11 +757,12 @@ fun AlertDialogs() {
 }
 
 @Composable
-fun Bars(){
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(Color.DarkGray)
-    ){
+private fun Bars() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.DarkGray)
+    ) {
         Row (
             modifier = Modifier
                 .align(Alignment.TopCenter)
@@ -757,131 +770,168 @@ fun Bars(){
                 .background(Color.Black)
                 .padding(10.dp, 50.dp, 10.dp, 10.dp),
             horizontalArrangement = Arrangement.SpaceBetween
-        ){
-            Icon(Icons.Filled.Menu,
-                contentDescription = "",
-                tint = Color.White)
-            Text(text = "App Title",
+        )
+        {
+            Icon(Icons.Filled.Menu, contentDescription = "", tint = Color.White)
+            Text(
+                text ="App Title",
                 color = Color.White,
                 fontWeight = FontWeight.Bold,
-                fontSize = 20.sp)
-            Icon(Icons.Filled.Settings,
-                contentDescription = "",
-                tint = Color.White)
+                fontSize = 20.sp
+            )
+            Icon(Icons.Filled.Settings, contentDescription = "", tint = Color.White)
         }
-        var post = arrayOf(
-            PostModel(1,"Tittle 1", "Text 1", painterResource(R.drawable.cipher)),
-            PostModel(2,"Tittle 2", "Text 2", painterResource(R.drawable.cipher)),
-            PostModel(3,"Tittle 3", "Text 3", painterResource(R.drawable.cipher)),
-            PostModel(4,"Tittle 4", "Text 4", painterResource(R.drawable.cipher)),
-            PostModel(5,"Tittle 5", "Text 5", painterResource(R.drawable.cipher)),
-            PostModel(6,"Tittle 6", "Text 6", painterResource(R.drawable.cipher)),
-            PostModel(7,"Tittle 7", "Text 7", painterResource(R.drawable.cipher)),
-            PostModel(8,"Tittle 8", "Text 8", painterResource(R.drawable.cipher)),
-            PostModel(9,"Tittle 9", "Text 9", painterResource(R.drawable.cipher)),
-            PostModel(10,"Tittle 10", "Text 10", painterResource(R.drawable.cipher)),
+        val post = arrayOf(
+            PostModel(1,"Title 1","Text 1",painterResource(R.drawable.cipher)),
+            PostModel(2,"Title 2","Text 2",painterResource(R.drawable.cipher)),
+            PostModel(3,"Title 3","Text 3",painterResource(R.drawable.cipher)),
+            PostModel(4,"Title 4","Text 4",painterResource(R.drawable.cipher)),
+            PostModel(5,"Title 5","Text 5",painterResource(R.drawable.cipher)),
+            PostModel(6,"Title 6","Text 6",painterResource(R.drawable.cipher)),
+            PostModel(7,"Title 7","Text 7",painterResource(R.drawable.cipher)),
+            PostModel(8,"Title 8","Text 8",painterResource(R.drawable.cipher)),
+            PostModel(9,"Title 9","Text 9",painterResource(R.drawable.cipher)),
+            PostModel(10,"Title 10","Text 10",painterResource(R.drawable.cipher)),
         )
         Column (
             modifier = Modifier
                 .align(Alignment.TopCenter)
                 .padding(10.dp, 90.dp, 10.dp, 50.dp)
                 .fillMaxSize()
-        ){
-            PostGrid(post)
+        )
+        {
             //Posts(post)
-            // PostCard(id = 1, title = "This is a Card",
-            //  text = "This is the Text Card", image = painterResource(R.drawable.android))
+            //PostCard(1,"This is a card title", "This is a card text", painterResource(R.drawable.descargar))
+            PostGrid(post)
         }
-
-        Row (
+        Row(
             modifier = Modifier
-                .align(Alignment.BottomEnd)
+                .align(Alignment.BottomCenter)
                 .fillMaxWidth()
                 .height(65.dp)
                 .background(Color.Black)
                 .padding(2.dp, 5.dp),
             horizontalArrangement = Arrangement.SpaceEvenly
         ){
-            Column (){
-                IconButton(onClick = {},
-                    modifier = Modifier.size(30.dp)) {
-                    Icon(Icons.Outlined.Home, contentDescription = "",
+            Column {
+                IconButton(onClick = {}, Modifier.size(30.dp)) {
+                    Icon(Icons.Outlined.Home,
+                        contentDescription = "",
                         tint = Color.White,
-                        modifier = Modifier
-                            .fillMaxSize()
+                        modifier = Modifier.fillMaxSize()
                     )
                 }
                 Text(text = "Home", color = Color.White)
             }
-            Column (){
-                IconButton(onClick = {},
-                    modifier = Modifier.size(30.dp)) {
-                    Icon(Icons.Outlined.Call, contentDescription = "",
+            Column {
+                IconButton(onClick = {}, Modifier.size(30.dp)) {
+                    Icon(Icons.Outlined.Search,
+                        contentDescription = "",
                         tint = Color.White,
-                        modifier = Modifier
-                            .fillMaxSize()
+                        modifier = Modifier.fillMaxSize()
                     )
                 }
-                Text(text = "Call", color = Color.White)
+                Text(text = "Search", color = Color.White)
             }
-            Column (){
-                IconButton(onClick = {},
-                    modifier = Modifier.size(30.dp)) {
-                    Icon(Icons.Outlined.AccountCircle, contentDescription = "",
+            Column {
+                IconButton(onClick = {}, Modifier.size(30.dp)) {
+                    Icon(Icons.Outlined.Person,
+                        contentDescription = "",
                         tint = Color.White,
-                        modifier = Modifier
-                            .fillMaxSize()
+                        modifier = Modifier.fillMaxSize()
                     )
                 }
-                Text(text = "Account", color = Color.White)
+                Text(text = "Profile", color = Color.White)
             }
-            Column (){
-                IconButton(onClick = {},
-                    modifier = Modifier.size(30.dp)) {
-                    Icon(Icons.Outlined.Face, contentDescription = "",
+            Column {
+                IconButton(onClick = {}, Modifier.size(30.dp)) {
+                    Icon(Icons.Outlined.Notifications,
+                        contentDescription = "",
                         tint = Color.White,
-                        modifier = Modifier
-                            .fillMaxSize()
+                        modifier = Modifier.fillMaxSize()
                     )
                 }
-                Text(text = "Friends", color = Color.White)
+                Text(text = "Alerts", color = Color.White)
             }
-            Column (){
-                IconButton(onClick = {},
-                    modifier = Modifier.size(30.dp)) {
-                    Icon(Icons.Outlined.Settings, contentDescription = "",
+            Column {
+                IconButton(onClick = {}, Modifier.size(30.dp)) {
+                    Icon(Icons.Outlined.Call,
+                        contentDescription = "",
                         tint = Color.White,
-                        modifier = Modifier
-                            .fillMaxSize()
+                        modifier = Modifier.fillMaxSize()
                     )
                 }
-                Text(text = "Settings", color = Color.White)
+                Text(text = "Contact", color = Color.White)
             }
         }
     }
 }
 
 @Composable
-fun Posts(arrayPosts: Array<PostModel>) {
+fun Posts(arrayPosts:Array<PostModel>, adaptive:String){
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
     ) {
         items(arrayPosts){ post->
-            PostCard(post.id, post.title, post.text, post.image)
+            when(adaptive) {
+                "PhoneP" -> {
+                    PostCardCompact(post.id, post.title, post.title, post.image)
+                }
+                "PhoneL" -> {
+                    PostCard(post.id, post.title, post.title, post.image)
+                }
+            }
         }
     }
 }
 
 @Composable
-fun PostGrid(arrayPosts: Array<PostModel>){
+fun PostGrid(arrayPosts:Array<PostModel>) {
     LazyVerticalGrid(
         columns = GridCells.Adaptive(minSize = 128.dp),
         modifier = Modifier
             .fillMaxSize()
     ) {
-        items(arrayPosts) { post->
-            PostCard(post.id, post.title, post.text, post.image)
+        items(arrayPosts){ post->
+            PostCard(post.id, post.title, post.title, post.image)
         }
+    }
+}
+
+@Preview(showBackground = true, device ="spec:id=reference_tablet,shape=Normal,width=1280,height=800,unit=dp,dpi=240")
+@Composable
+fun Adaptive() {
+    var WindowsSize = currentWindowAdaptiveInfo().windowSizeClass
+    var height = currentWindowAdaptiveInfo().windowSizeClass.windowHeightSizeClass
+    var width = currentWindowAdaptiveInfo().windowSizeClass.windowWidthSizeClass
+
+    // Compact width < 600dp Phone portrait
+    // Medium width >= 600dp < 840dp Tablets portrait
+    // Expanded width >840dp Tablet landscape
+
+    // Compact height < 480dp Phone landscape
+    // Medium height >= 480dp < 900dp Tablet landscape or Phone portrait
+    // Expanded height >900dp Tablet in portrait
+
+    val post = arrayOf(
+        PostModel(1, "Title 1", "Text 1", painterResource(R.drawable.cipher)),
+        PostModel(2, "Title 2", "Text 2",painterResource(R.drawable.cipher)),
+        PostModel(3, "Title 3", "Text 3", painterResource(R.drawable.cipher)),
+        PostModel(4, "Title 4", "Text 4", painterResource(R.drawable.cipher)),
+        PostModel(5, "Title 5", "Text 5", painterResource(R.drawable.cipher)),
+        PostModel(6, "Title 6", "Text 6", painterResource(R.drawable.cipher)),
+        PostModel(7, "Title 7", "Text 7", painterResource(R.drawable.cipher)),
+        PostModel(8, "Title 8", "Text 8", painterResource(R.drawable.cipher)),
+        PostModel(9, "Title 9", "Text 9", painterResource(R.drawable.cipher)),
+        PostModel(10, "Title 10", "Text 10", painterResource(R.drawable.cipher)),
+    )
+    if (width == WindowWidthSizeClass.COMPACT) {
+        Posts(post, "PhoneP")
+    } else if (height == WindowHeightSizeClass.COMPACT) {
+        Posts(post, "PhoneL")
+    } else {
+        Posts(post, "PhoneL")
+        //Text(text = WindowsSize.toString())
     }
 }
