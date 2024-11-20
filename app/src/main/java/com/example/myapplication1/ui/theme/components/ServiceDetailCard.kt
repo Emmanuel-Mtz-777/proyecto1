8532084d8d6bbbe93ce9937ecb83f2b7eb2ee15c
Manuel <hemmanuelmtz777@gmail.com>
2024-11-20 07:54:23 -0600

    Feat sparkles (ServiceCard.kt)(ServiceDetailCard.kt)Edith the content of servieCard and create ServiceDetailCard.kt

---
+++ app/src/main/java/com/example/myapplication1/ui/theme/components/ServiceDetailCard.kt
@@ -0,0 +1,79 @@
+package com.example.myapplication1.ui.theme.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.ClipboardManager
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.myapplication1.R
+
+@Composable
+fun ServiceDetailCard(
+    id:Int,
+    name:String,
+    username:String,
+    password:String,
+    description:String,
+    imageUrl:String?,
+    onEditClick: ()->Unit
+){
+val clipboardManager: ClipboardManager = LocalClipboardManager.current
+Column {
+    Row (
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(120.dp)
+            .padding(10.dp),
+        Arrangement.Absolute.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        AsyncImage(
+            modifier = Modifier
+                .width(100.dp)
+                .height(100.dp),
+            model = imageUrl,
+            error = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "Service Logo",
+            contentScale = ContentScale.FillBounds)
+        Text(text = name,
+            color = colorResource(R.color.teal_700),
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold)
+        Row (
+            modifier = Modifier,
+            Arrangement.End
+        ){
+            IconButton(
+                modifier = Modifier.padding(20.dp,0.dp,0.dp,0.dp),
+                onClick = { onEditClick()}) {
+                Icon(imageVector = Icons.Filled.Edit, contentDescription = "Select date")
+
+            }
+        }
+
+    }
+    HorizontalDivider()
+}
+}
